0b01fd98d35eba0feb587f86d0b8e73d4fd2024d
Vincent Macri <developer@vincemacri.ca>
2018-08-23 13:14:52 -0400

    Mess around.

--- Collatz Conjecture/ReverseCollatz.py
+++ Collatz Conjecture/ReverseCollatz.py
@@ -1,26 +1,33 @@
 START = 1
-END = 20
+END = 15
 
 collatzMap = set()
 
 def chain(a, b):
 	return "{} -> {}".format(a, b)
 
-def printChain(a, b):
-	print("\t{};".format(chain(a, b)))
+def addToChain(a, b):
+	collatzMap.add((a, b))
 
 def reverseCollatz(n, a):
 	a -= 1
-	printChain(2 * n, n)
-	if a > 0:
-		reverseCollatz(2 * n, a)
+
+	evenChain = (2 * n, n)
+	if evenChain not in collatzMap:
+		addToChain(2 * n, n)
+		if a > 0:
+			reverseCollatz(2 * n, a)
+
 	oddReverse = (n - 1) / 3
-	if oddReverse % 2 == 1 and oddReverse > 0:
-		printChain(int(oddReverse), n)
+	oddChain = (int(oddReverse), n)
+	if oddReverse % 2 == 1 and oddReverse > 0 and oddChain not in collatzMap:
+		addToChain(int(oddReverse), n)
 		if a > 0:
 			reverseCollatz(int(oddReverse), a)
 
 
 print("strict digraph C {")
 reverseCollatz(1, END)
+for c in collatzMap:
+	print(chain(c[0], c[1]))
 print("}")
